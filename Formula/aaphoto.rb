277927929f2bd2c84aae0df5d550e87de0dc856d
Takeshi Kakeda <takeshi@giantech.jp>
2025-09-01 13:31:15 +0900

    v0.45.1

--- Formula/aaphoto.rb
+++ Formula/aaphoto.rb
@@ -1,10 +1,10 @@
 class Aaphoto < Formula
   desc "Auto Adjust Photo - command-line image manipulation tool for automatic color correction"
   homepage "http://log69.com/aaphoto_en.html"
-  url "https://github.com/kkd/aaphoto/archive/v0.45.tar.gz"
-  sha256 "571fba9bcbe2d381bcf013a0d945f5fdaf7b054e477ab18a2f9a4e2efcf09222"
+  url "https://github.com/kkd/aaphoto/archive/v0.45.1.tar.gz"
+  sha256 "fe6eb88674900c0623e649d7d3e40f88810b09299427f26007f7b9be8e1d0c30"
   license "GPL-3.0-or-later"
-  version "0.45"
+  version "0.45.1"
 
   depends_on "autoconf" => :build
   depends_on "automake" => :build
